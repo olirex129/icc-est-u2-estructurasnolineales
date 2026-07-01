0a9c280fc54eb4290b29ac6a5a615453ca74b81d
Oliver Valdiviezo <olivervaldiviezo67@gmail.com>
2026-07-01 17:56:27 -0500

    UPD: practica de la prueba ejercicio 2

--- src/eva/PersonaController.java
+++ src/eva/PersonaController.java
@@ -1,7 +1,10 @@
 package eva;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class PersonaController {
@@ -27,16 +30,32 @@ public class PersonaController {
     return personasUmbral;
 }
 
-    // public Map<String,Set<String>> agruparPorEdad(List<Persona> personas){
-
-    // String nombre = "Juan Perez"; // para sacar el primer nombre usamos el split
-    // // cuando encuentre un espacio va a divir la palabra en un arreglo de Strings
-    // // va a retornar, "Juan","Perez" y para quedarnos con el primer nombre sacamos la posicion 0.
+    public Map<String,Set<String>> agruparPorEdad(List<Persona> personas){
+ 
+    // para sacar el primer nombre usamos el split
+    // cuando encuentre un espacio va a divir la palabra en un arreglo de Strings
+    // va a retornar, "Juan","Perez" y para quedarnos con el primer nombre sacamos la posicion 0.
     // String[] palabras = nombre.split(" ");
     // String primerNombre = palabras[0];
     // String pNombre = nombre.split("")[0];
-    // Map<String,Set<String>> mapa = new TreeMap<>();
-    // mapa.put(OVEN,new LinkedHashSet);
-    // }
+    Map<String,Set<String>> mapa = new TreeMap<>();
+    Set<String> joven = new LinkedHashSet<>();
+    Set<String> adulto = new LinkedHashSet<>();
+    Set<String> mayor = new LinkedHashSet<>();
+    mapa.put("joven", joven);
+    mapa.put("adulto", adulto);
+    mapa.put("mayor", mayor);
+    for (Persona persona : personas) {
+        if(persona.getEdad()<30)
+            joven.add(persona.getNombre().split(" ")[0].toLowerCase());
+        else if(persona.getEdad()<60)
+            adulto.add(persona.getNombre().split(" ")[0].toLowerCase());
+        else{
+            mayor.add(persona.getNombre().split(" ")[0].toLowerCase());
+        }
+    }
+    return mapa;
+    
+    }
     
 }
--- src/eva/Readme.md
+++ src/eva/Readme.md
@@ -6,4 +6,6 @@
 - **Fecha:** [01/07/2026]
 
 ---
-Utilizo el treeSet porque me permite modificar el metodo por el cual voy a ordenar y tambien porque puedo tenerlo organizado
+En el primer problema utilizo el treeSet porque me permite modificar el metodo por el cual voy a ordenar y tambien porque puedo tenerlo organizado
+
+En el segundo ejercicio utilizo un Map con la key en string, porque al tener un Map de tipo Tree map las Key se van a ordenar de forma natural con los tipo String (alfabeticamente), y el valor un Set de linkedHashSet porque esta lista naturalmente evita que un String del mismo tipo pueda estar en esta misma lista y simplemente no lo agrega evitando duplicados, ademas al usarlo con Linked(nodos) conservaran el orden por el que entra.5 
